b4f8aa1493e3e0bed4bbe2d8d41e36b556a83661
ezazee <yukiirima123@gmail.com>
2023-05-05 00:13:23 +0700

    first

---
+++ src/components/dashboard/RecentTransactions.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import DashboardCard from '../../../src/components/shared/DashboardCard';
+import {
+  Timeline,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator,
+  TimelineDot,
+  TimelineConnector,
+  TimelineContent,
+  timelineOppositeContentClasses,
+} from '@mui/lab';
+import { Link, Typography } from '@mui/material';
+
+const RecentTransactions = () => {
+  return (
+    <DashboardCard title="Riwayat Transaksi">
+      <>
+        <Timeline
+          className="theme-timeline"
+          nonce={undefined}
+          onResize={undefined}
+          onResizeCapture={undefined}
+          sx={{
+            p: 0,
+            mb: '-10px',
+            '& .MuiTimelineConnector-root': {
+              width: '1px',
+              backgroundColor: '#efefef'
+            },
+            [`& .${timelineOppositeContentClasses.root}`]: {
+              flex: 0.5,
+              paddingLeft: 0,
+            },
+          }}
+        >
+          <TimelineItem>
+            <TimelineOppositeContent>09:30 am</TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="primary" variant="outlined" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>Pembayaran telah dikirim oleh Doy Senilai "Rp.250.000"</TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>10:00 am</TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="secondary" variant="outlined" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography fontWeight="600">Pesanan telah diterima oleh pemesan</Typography>{' '}
+              <Link href="/" underline="none">
+                #CD-BRG1
+              </Link>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>12:00 am</TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="success" variant="outlined" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>Pembayaran telah dikirim oleh Zae senilai "Rp.100.000"</TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>09:30 am</TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="warning" variant="outlined" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography fontWeight="600">Pesanan telah diterima oleh pemesan</Typography>{' '}
+              <Link href="/" underline="none">
+                #CD-BRG2
+              </Link>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>12:00 am</TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="success" variant="outlined" />
+            </TimelineSeparator>
+            <TimelineContent>Pembayaran telah dikirim oleh Syuu senilai "Rp.500.000"</TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </>
+    </DashboardCard>
+  );
+};
+
+export default RecentTransactions;
